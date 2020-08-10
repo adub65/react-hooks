02425fdce08d82c5e7c8811ebe024553717ce4c9
Aaron Wilson <adub65@gmail.com>
2020-08-10 14:37:25 -0400

    move handleSubmit logic out of form and into const

--- src/forms/AddUserForm.js
+++ src/forms/AddUserForm.js
@@ -10,20 +10,32 @@ const AddUserForm = props => {
 		setUser({ ...user, [name]: value })
 	}
 
+	const handleSubmit = event => {
+		event.preventDefault()
+		if (!user.name || !user.username)
+			return
+			props.addUser(user)
+			setUser(initialFormState)
+	}
+
 	return (
 		<form
-			onSubmit={event => {
-				event.preventDefault()
-				if (!user.name || !user.username) return
-
-				props.addUser(user)
-				setUser(initialFormState)
-			}}
+			onSubmit={handleSubmit}
 		>
 			<label>Name</label>
-			<input type="text" name="name" value={user.name} onChange={handleInputChange} />
+			<input
+				type="text"
+				name="name"
+				value={user.name}
+				onChange={handleInputChange}
+			/>
+
 			<label>Username</label>
-			<input type="text" name="username" value={user.username} onChange={handleInputChange} />
+			<input
+				type="text"
+				name="username"
+				value={user.username}
+				onChange={handleInputChange} />
 			<button>Add new user</button>
 		</form>
 	)
