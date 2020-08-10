e4f2bb7c291578fae776be013bb1a78c25675936
Aaron Wilson <adub65@gmail.com>
2020-08-10 14:40:05 -0400

    handleSubmit logic taken from EditUserForm into const

--- src/forms/EditUserForm.js
+++ src/forms/EditUserForm.js
@@ -17,13 +17,14 @@ const EditUserForm = props => {
     setUser({ ...user, [name]: value })
   }
 
+  const handleSubmit = event => {
+    event.preventDefault()
+    props.updateUser(user.id, user)
+  }
+
   return (
     <form
-      onSubmit={event => {
-        event.preventDefault()
-
-        props.updateUser(user.id, user)
-      }}
+      onSubmit={handleSubmit}
     >
       <label>Name</label>
       <input type="text" name="name" value={user.name} onChange={handleInputChange} />
